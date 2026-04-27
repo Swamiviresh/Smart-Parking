8109f4d8f745b19cae10e4dadb52d655eca778f0
Swamiviresh <swamiviresh2007@gmail.com>
2026-04-27 23:05:30 +0530

    Fixed API base URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smartparking.live", // 👈 IMPORTANT
+  baseURL: "https://smartparking.live/api", // 👈 IMPORTANT
 });
 
 export default api;
