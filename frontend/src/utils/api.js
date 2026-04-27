46db93cae7bbe165d88dfe283489e25f45d9814c
Swamiviresh <swamiviresh2007@gmail.com>
2026-04-27 22:58:42 +0530

    Fixed frontend API base URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,26 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: "https://smartparking.live", // 👈 IMPORTANT
 });
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.clear();
-      window.location.href = '/';
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default api;
